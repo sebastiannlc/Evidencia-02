ac073f513010a7a864295badc9e1ddd825f577bc
1uisardinh0 <l.villalobos03@ufromail.cl>
2025-09-01 18:59:29 -0400

    Programa que imprime por consola el mensaje "Hola Mundo" y ademas mi nombre

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -1,6 +1,6 @@
 public class HolaMundo {
     public static void main(String[] args) {
         System.out.println("¡Hola Mundo!");
-        System.out.println("Soy: Luis Armando Villalobos Morales");
+        System.out.println("Soy: Luis Armando Villalobos Morales.");
     }
 }
